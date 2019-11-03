ba0dac1fe5cde1205ce544b4e2cf097f3ff9d033
jc3wish <jc3wish@126.com>
2019-11-03 18:35:04 +0800

    time format

--- manager/public/js/flow.js
+++ manager/public/js/flow.js
@@ -49,8 +49,8 @@ var FlowClass  = {
         var h = time.getHours();
         var mm = time.getMinutes();
         var s = time.getSeconds();
-        return this.add0(h) + ':' + this.add0(mm) + ':' + this.add0(s);
-        //return y + '-' + this.add0(m) + '-' + this.add0(d) + ' ' + this.add0(h) + ':' + this.add0(mm) + ':' + this.add0(s);
+        //return this.add0(h) + ':' + this.add0(mm) + ':' + this.add0(s);
+        return y + '-' + this.add0(m) + '-' + this.add0(d) + ' ' + this.add0(h) + ':' + this.add0(mm) + ':' + this.add0(s);
     },
 
     init_data: function () {
